ff96436bc62bfdb9ebd09902818845c4a28f9c7d
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-17 13:36:21 +0500

    Update LoginController.js

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -3,41 +3,59 @@ import { Student } from "../Models/Student.model.js";
 import { Teacher } from "../Models/Teacher.model.js";
 
 export const TeacherLogin = (req, res) => {
-  let { email, Password } = req.body;
-  Teacher.findByEmail(email, (err, result) => {
-    console.log(result);
-    if (err) console.log(err);
-    if (result.Found === true && result[0].teacher_password === Password) {
-      let obj = { id: result[0].teacher_id }; // yahan par kam hoga
-      let token = CreateToken(obj, "Teacher");
-      res.cookie("token", token, { secure: false, httpOnly: false });
-      res.send({ token: token }).status(200);
-    } else {
-      res.status(401).json({
-        success: false,
-        message: "Invalid email OR Password",
-        result: {},
-      });
-    }
-  });
+  try {
+    let { email, Password } = req.body;
+    Teacher.findByEmail(email, (err, result) => {
+      console.log(result);
+      if (err) throw err;
+      if (result.Found === true && result[0].teacher_password === Password) {
+        let obj = { id: result[0].teacher_id };
+        let token = CreateToken(obj, "Teacher");
+        res.cookie("token", token, { secure: false, httpOnly: false });
+        res.send({ token: token }).status(200);
+      } else {
+        res.status(401).json({
+          success: false,
+          message: "Invalid email OR Password",
+          result: {},
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
 };
 
 export const StudentLogin = (req, res) => {
-  let { email, Password } = req.body;
-  Student.findByEmail(email, (err, result) => {
-    console.log(result);
-    if (err) console.log(err);
-    if (result.Found === true && result[0].student_password === Password) {
-      let obj = { id: result[0].student_id }; // yahan par kam hoga
-      let token = CreateToken(obj, "Student");
-      res.cookie("token", token, { secure: false, httpOnly: false });
-      res.send({ token: token }).status(200);
-    } else {
-      res.status(401).json({
-        success: false,
-        message: "Invalid email OR Password",
-        result: {},
-      });
-    }
-  });
+  try {
+    let { email, Password } = req.body;
+    Student.findByEmail(email, (err, result) => {
+      console.log(result);
+      if (err) throw err;
+      if (result.Found === true && result[0].student_password === Password) {
+        let obj = { id: result[0].student_id };
+        let token = CreateToken(obj, "Student");
+        res.cookie("token", token, { secure: false, httpOnly: false });
+        res.send({ token: token }).status(200);
+      } else {
+        res.status(401).json({
+          success: false,
+          message: "Invalid email OR Password",
+          result: {},
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred",
+      error: error.message,
+    });
+  }
 };
