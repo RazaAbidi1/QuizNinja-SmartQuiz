145f3f1d8796301261c285f8dc2b3d862788bd36
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-19 19:56:35 +0500

    Comments added LoginController.js

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -6,27 +6,30 @@ export const TeacherLogin = (req, res) => {
   try {
     let { email, Password } = req.body;
     Teacher.findByEmail(email, (err, result) => {
-      console.log(result);
+      console.log(result); // Find the teacher by email using the Teacher model
       if (err) throw err;
       if (result.Found === true && result[0].teacher_password === Password) {
+        // If teacher is found and the provided password matches, create a token and send it in the response
         let obj = { id: result[0].teacher_id };
         let token = CreateToken(obj, "Teacher");
         res.cookie("token", token, { secure: false, httpOnly: false });
         res.send({ token: token }).status(200);
       } else {
+        
         res.status(401).json({
           success: false,
           message: "Invalid email OR Password",
-          result: {},
+          result: {},// Send error response if the teacher is not found or the password is incorrect
         });
       }
     });
   } catch (error) {
+    
     console.log(error);
     res.status(500).json({
       success: false,
       message: "An error occurred",
-      error: error.message,
+      error: error.message,// Catch any unexpected errors and send an error response
     });
   }
 };
@@ -34,28 +37,33 @@ export const TeacherLogin = (req, res) => {
 export const StudentLogin = (req, res) => {
   try {
     let { email, Password } = req.body;
+    // Find the student by email using the Student model
     Student.findByEmail(email, (err, result) => {
       console.log(result);
       if (err) throw err;
       if (result.Found === true && result[0].student_password === Password) {
+        // If student is found and the provided password matches, create a token and send it in the response
         let obj = { id: result[0].student_id };
         let token = CreateToken(obj, "Student");
         res.cookie("token", token, { secure: false, httpOnly: false });
         res.send({ token: token }).status(200);
       } else {
+        
         res.status(401).json({
           success: false,
           message: "Invalid email OR Password",
-          result: {},
+          result: {},// Send error response if the student is not found or the password is incorrect
         });
       }
     });
   } catch (error) {
+   
     console.log(error);
     res.status(500).json({
       success: false,
       message: "An error occurred",
       error: error.message,
+       // Catch any unexpected errors and send an error response
     });
   }
 };
