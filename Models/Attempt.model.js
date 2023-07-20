c33c1ccf0b4be3a191e69c94c2dfa0db1fa772ad
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-20 17:29:45 +0500

    comment added inAttempt.model.js

--- Models/Attempt.model.js
+++ Models/Attempt.model.js
@@ -1,9 +1,15 @@
+// Import the sql connection from the DBconfig module
 import { sql } from "../Config/DBconfig";
+
+// Class representing an 'Attempt' object
 export class Attempt {
   constructor(attempt) {
     this.attempt = attempt;
   }
+
+  // Method to create an 'Attempt' record in the database
   create = (cb) => {
+    // Execute the SQL query to insert the 'Attempt' object into the 'Attempt' table
     sql.query("insert into Attempt set ?", [this.attempt], cb);
   };
 }
