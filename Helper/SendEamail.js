1d50efe8db4a3a121b4fb622c53391f5a776ed88
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-20 15:33:36 +0500

    Comments added in Update SendEamail.js

--- Helper/SendEamail.js
+++ Helper/SendEamail.js
@@ -1,25 +1,32 @@
+// Import the nodemailer library
 import nodemailer from "nodemailer";
 
+// Create a nodemailer transporter with Gmail service and authentication credentials
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+  service: "Gmail", // The email service to be used (in this case, Gmail)
   auth: {
-    user: process.env.email,
-    pass: process.env.Password,
+    user: process.env.email, // The email address used for authentication (provided through environment variable)
+    pass: process.env.Password, // The password used for authentication (provided through environment variable)
   },
 });
 
+// Send an email using the nodemailer transporter
 export const SendEmail = (To, Subject, Body, cb) => {
   const mailOptions = {
-    from: process.env.email,
-    to: To,
-    subject: Subject,
-    text: Body,
+    from: process.env.email, // The sender's email address (provided through environment variable)
+    to: To, // The recipient's email address
+    subject: Subject, // The subject of the email
+    text: Body, // The body content of the email
   };
+
+  // Send the email using the nodemailer transporter
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
+      // If there's an error while sending the email, log the error and call the callback function with the error
       console.log(error);
       cb(error, null);
     } else {
+      // If the email is sent successfully, log the response and call the callback function with the information about the sent email
       console.log("Email sent: " + info.response);
       cb(null, info);
     }
