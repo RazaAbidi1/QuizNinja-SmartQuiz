427bdfdc15ac2f7c6c3c4be5995cd09cf4d5720c
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-20 15:36:22 +0500

    Comments in Update ShuffleArray.js

--- Helper/ShuffleArray.js
+++ Helper/ShuffleArray.js
@@ -1,6 +1,8 @@
+
+// Questions will be displayed in random order on every quiz attempt
 export function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
-  } // Shuffling bilkul sahi hai DONOT CHANGE IT
+  } 
 }
