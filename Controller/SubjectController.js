c2c486d9606e9e0065ed1f387b81728e321160ef
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-20 13:16:18 +0500

    comments added in SubjectController.js

--- Controller/SubjectController.js
+++ Controller/SubjectController.js
@@ -1,25 +1,37 @@
+// Import the Subject model
 import { Subject } from "../Models/Subject.model.js";
 
-export const Add_Subject = (req, res) => {
-  return res.status(200).send({ msg: "Work under Progress" });
-};
 
+// View all subjects
 export const View_Subject = (req, res) => {
+  // Retrieve all subjects using the Subject model's getAll method
   Subject.getAll((err, result) => {
-    if (err) res.send({ err }).status(403);
-    else res.send({ result }).status(200);
+    if (err) {
+      // If there's an error, send the error response with a 403 status code(the server understands the request but can't provide additional access)
+      res.send({ err }).status(403);
+    } else {
+      // If successful, send the result as the response with a 200 status code
+      res.send({ result }).status(200);
+    }
   });
 };
 
-//To Get corresponding teachers for a Subject
+// View corresponding teachers for a subject
 export const View_Teachers = (req, res) => {
   const { Subject_name } = req.body;
   if (Subject_name) {
+    // If the subject name is provided in the request body, proceed to find teachers for that subject
     Subject.Find_Teacher(Subject_name, (err, result) => {
-      if (err) res.send({ err }).status(403);
-      else res.send({ result }).status(200);
+      if (err) {
+        // If there's an error, send the error response with a 403 status code (the server understands the request but can't provide additional access)
+        res.send({ err }).status(403);
+      } else {
+        // If successful, send the result as the response with a 200 status code
+        res.send({ result }).status(200);
+      }
     });
   } else {
-    res.send({ err: "Subejct Name Required" }).status(401);
+    // If the subject name is not provided in the request body, send an error response with a 401 status code(unauthorized response)
+    res.send({ err: "Subject Name Required" }).status(401);
   }
 };
