0830c8f0b90c41644f42ab48159e80696bb58708
Raza Ali Abidi <92652904+RazaAbidi1@users.noreply.github.com>
2023-07-20 15:39:56 +0500

    Comments added  toSeconds.js

--- Helper/toSeconds.js
+++ Helper/toSeconds.js
@@ -1,8 +1,10 @@
+// Convert a time string to seconds
 export const toSeconds = (str) => {
-  // Split the time string into hours, minutes, and seconds
+  // Split the time string into hours, minutes, and seconds using the ":" as the separator
   const [hours, minutes, seconds] = str.split(":");
 
-  // Calculate the total seconds
   const totalSeconds = +hours * 3600 + +minutes * 60 + +seconds;
+
+  // Return the total number of seconds
   return totalSeconds;
 };
